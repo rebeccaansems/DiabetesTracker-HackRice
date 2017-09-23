b03f17f5173a0b9eae05d7cb6aa299a26842d26d
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-09-23 01:08:16 -0300

    Added basics of a settings page

--- app/src/main/java/rebeccaansems/diabetestracker/Settings.java
+++ app/src/main/java/rebeccaansems/diabetestracker/Settings.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.settings);
 
 
     }
