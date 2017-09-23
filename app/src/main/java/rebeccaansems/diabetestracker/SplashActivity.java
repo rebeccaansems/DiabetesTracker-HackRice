3ed40a469831b714808a053bb3bd2f066025026a
10001shh <sghsri@gmail.com>
2017-09-23 13:14:33 -0500

    added spash screen & icon

---
+++ app/src/main/java/rebeccaansems/diabetestracker/SplashActivity.java
@@ -0,0 +1,17 @@
+package rebeccaansems.diabetestracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
