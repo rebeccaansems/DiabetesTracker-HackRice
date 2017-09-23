3b400b66b0805cf3d663a87b68be435bac3b9e8e
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-09-23 14:52:38 -0300

    Added more functionality to adding blood glucose screen

--- app/src/main/java/rebeccaansems/diabetestracker/BloodGlucoseInput.java
+++ app/src/main/java/rebeccaansems/diabetestracker/BloodGlucoseInput.java
@@ -1,35 +1,30 @@
 package rebeccaansems.diabetestracker;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import java.util.Date;
 
-/**
- * Created by Rebecca Ansems on 2017-09-23.
- */
+import java.util.Date;
 
 public class BloodGlucoseInput extends AppCompatActivity
 {
     EditText currentBloodSugar;
     CheckBox didExercise, isSick, isHoromones;
-    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inputbloodsugar);
-
-        setTitle("Input");
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         currentBloodSugar = (EditText) findViewById(R.id.e_bloodSugarValue);
 
@@ -41,14 +36,49 @@ public class BloodGlucoseInput extends AppCompatActivity
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 addData();
-                gotoMainScreen();
             }
         });
     }
 
     void addData(){
-        BloodSugarDataPoint bsdp = new BloodSugarDataPoint(Integer.parseInt(currentBloodSugar.getText().toString()),
+        BloodSugarDataPoint bsdp = new BloodSugarDataPoint(Float.parseFloat(currentBloodSugar.getText().toString()),
                 new Date(), didExercise.isChecked(), isSick.isChecked(), isHoromones.isChecked());
+        bsdp.save();
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        float highBlood = Float.parseFloat(sharedPref.getString("highBlood", "10.0"));
+
+        if(bsdp.bloodSugarValue > highBlood && bsdp.getId() > 3 &&
+                BloodSugarDataPoint.findById(BloodSugarDataPoint.class, bsdp.getId()-1).bloodSugarValue > highBlood&&
+                BloodSugarDataPoint.findById(BloodSugarDataPoint.class, bsdp.getId()-2).bloodSugarValue > highBlood){
+
+            AlertDialog.Builder builder;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+            } else {
+                builder = new AlertDialog.Builder(this);
+            }
+            builder.setTitle("High Blood Sugars")
+                    .setMessage("You have had high blood sugars for the last three readings, " +
+                            "consider testing for ketones or changing your insulin site.")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            gotoMainScreen();
+                        }
+                    })
+                    .setNegativeButton("Contact Doctor", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            gotoMainScreen();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+        else {
+            gotoMainScreen();
+        }
     }
 
     void gotoMainScreen(){
