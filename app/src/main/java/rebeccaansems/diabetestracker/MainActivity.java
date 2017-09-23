4e80b4af0c92105222839a99701de6389b0fd90e
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-09-23 02:27:12 -0300

    Added functionality to settings page

--- app/src/main/java/rebeccaansems/diabetestracker/MainActivity.java
+++ app/src/main/java/rebeccaansems/diabetestracker/MainActivity.java
@@ -1,20 +1,35 @@
 package rebeccaansems.diabetestracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openedMainScreen();
+    }
 
+    void openedMainScreen(){
+        Button gotoSettings = (Button) findViewById(R.id.b_gotoSettings);
+        gotoSettings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                gotoSettings();
+            }
+        });
+    }
 
+    void gotoSettings(){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/rebeccaansems/diabetestracker/Settings.java
+++ app/src/main/java/rebeccaansems/diabetestracker/Settings.java
@@ -1,20 +1,83 @@
 package rebeccaansems.diabetestracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
+
+    EditText lowBlood, highBlood, veryHighBlood;
+    EditText breakfastTime, lunchTime, supperTime, nightTime, lateNightTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        lowBlood = (EditText)findViewById(R.id.e_lowBlood);
+        highBlood = (EditText)findViewById(R.id.e_highBlood);
+        veryHighBlood = (EditText)findViewById(R.id.e_veryHighBlood);
 
+        breakfastTime = (EditText)findViewById(R.id.e_breakfastTime);
+        lunchTime = (EditText)findViewById(R.id.e_lunchTime);
+        supperTime = (EditText)findViewById(R.id.e_supperTime);
+        nightTime = (EditText)findViewById(R.id.e_nightTime);
+        lateNightTime = (EditText)findViewById(R.id.e_lateNightTime);
+
+        LoadSavedData();
+
+        Button submitSettings = (Button) findViewById(R.id.b_submitSettings);
+        submitSettings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                saveData();
+                gotoMainScreen();
+            }
+        });
+    }
+
+    void LoadSavedData(){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        lowBlood.setText(sharedPref.getString("lowBlood", "4.0"), TextView.BufferType.EDITABLE);
+        highBlood.setText(sharedPref.getString("highBlood", "10.0"), TextView.BufferType.EDITABLE);
+        veryHighBlood.setText(sharedPref.getString("veryHighBlood", "15.0"), TextView.BufferType.EDITABLE);
+
+        breakfastTime.setText(sharedPref.getString("breakfastTime", "09:00"), TextView.BufferType.EDITABLE);
+        lunchTime.setText(sharedPref.getString("lunchTime", "12:00"), TextView.BufferType.EDITABLE);
+        supperTime.setText(sharedPref.getString("supperTime", "17:00"), TextView.BufferType.EDITABLE);
+        nightTime.setText(sharedPref.getString("nightTime", "23:00"), TextView.BufferType.EDITABLE);
+        lateNightTime.setText(sharedPref.getString("lateNightTime", "03:00"), TextView.BufferType.EDITABLE);
+    }
+
+    void saveData(){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString("lowBlood", lowBlood.getText().toString());
+        editor.putString("highBlood", highBlood.getText().toString());
+        editor.putString("veryHighBlood", veryHighBlood.getText().toString());
+
+        editor.putString("breakfastTime", breakfastTime.getText().toString());
+        editor.putString("lunchTime", lunchTime.getText().toString());
+        editor.putString("supperTime", supperTime.getText().toString());
+        editor.putString("nightTime", nightTime.getText().toString());
+        editor.putString("lateNightTime", lateNightTime.getText().toString());
+
+        editor.commit();
+    }
+
+    void gotoMainScreen(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
