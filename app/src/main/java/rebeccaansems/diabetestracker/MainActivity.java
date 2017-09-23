15ce31714406a843447265e814acbb28645739bf
10001shh <sghsri@gmail.com>
2017-09-23 01:23:54 -0500

    Added Action Bar and icons

--- app/src/main/java/rebeccaansems/diabetestracker/MainActivity.java
+++ app/src/main/java/rebeccaansems/diabetestracker/MainActivity.java
@@ -1,35 +1,58 @@
 package rebeccaansems.diabetestracker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView mTextMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        openedMainScreen();
+        setTitle("SixTwelveSix");
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 
-    void openedMainScreen(){
-        Button gotoSettings = (Button) findViewById(R.id.b_gotoSettings);
-        gotoSettings.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                gotoSettings();
-            }
-        });
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.newentry:
+                return true;
+            case R.id.settings:
+                Intent intent = new Intent(this, Settings.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
-    void gotoSettings(){
-        Intent intent = new Intent(this, Settings.class);
-        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbutton, menu);
+        return true;
     }
 
 }
