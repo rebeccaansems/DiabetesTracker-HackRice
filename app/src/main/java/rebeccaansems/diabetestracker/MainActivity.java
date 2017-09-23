f5a20dcf7278f9342f8b02c13bb3cf10ed230f87
10001shh <sghsri@gmail.com>
2017-09-23 05:03:46 -0500

    added rough Chart

--- app/src/main/java/rebeccaansems/diabetestracker/MainActivity.java
+++ app/src/main/java/rebeccaansems/diabetestracker/MainActivity.java
@@ -2,6 +2,9 @@ package rebeccaansems.diabetestracker;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,11 +17,33 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener {
 
     private Toolbar toolbar;
+    protected HorizontalBarChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +52,92 @@ public class MainActivity extends AppCompatActivity {
         setTitle("six-twelve-six");
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mChart = (HorizontalBarChart) findViewById(R.id.chart1);
+        mChart.setOnChartValueSelectedListener(this);
+
+        // scaling can now only be done on x- and y-axis separately
+        mChart.setPinchZoom(false);
+        // draw shadows for each bar that show the maximum value
+        // mChart.setDrawBarShadow(true);
+
+        mChart.setDrawGridBackground(false);
+        XAxis xl = mChart.getXAxis();
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xl.setDrawAxisLine(true);
+        xl.setDrawGridLines(false);
+        xl.setLabelCount(0);
+        xl.setGranularity(10f);
+
+        YAxis yl = mChart.getAxisLeft();
+        yl.setDrawAxisLine(true);
+        yl.setDrawGridLines(false);
+        yl.setDrawLabels(false);
+        yl.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+//        yl.setInverted(true);
+
+        YAxis yr = mChart.getAxisRight();
+        yr.setDrawAxisLine(true);
+        yr.setDrawGridLines(false);
+        yr.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+//        yr.setInverted(true);
+
+        setData(7);
+        mChart.setFitBars(true);
+        mChart.animateY(2500);
+
+
+        Legend l = mChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setFormSize(8f);
+        l.setXEntrySpace(4f);
+    }
+
+    private void setData(int count) {
+
+        float barWidth = 5f;
+        float spaceForBar = 8f;
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+        for(int i = count; i>0; i--){
+            float val = (float)Math.random()*10;
+            if(val > 5){
+                yVals1.add(new BarEntry(i * spaceForBar, (float)(Math.random()*10),getResources().getDrawable(R.drawable.alert)
+                ));
+            }
+            else {
+                yVals1.add(new BarEntry(i * spaceForBar, (float) (Math.random() * 10)
+                ));
+            }
+        }
+
+
+        BarDataSet set1;
+
+        if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet)mChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals1);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(yVals1, "DataSet 1");
+
+            set1.setDrawIcons(true);
+            set1.setColor(getResources().getColor(R.color.colorPrimaryDark));
+            set1.setHighLightColor(getResources().getColor(R.color.colorAccent));
+      //      set1.setValueTypeface(Typeface.DEFAULT_BOLD);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+            dataSets.add(set1);
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(barWidth);
+            data.setValueTextSize(20);
+            data.setValueTextColor(getResources().getColor(R.color.colorLight));
+            data.setValueTypeface(Typeface.DEFAULT_BOLD);
+            mChart.setData(data);
+        }
     }
 
 
@@ -51,5 +162,14 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.actionbutton, menu);
         return true;
     }
+
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    };
 
 }
